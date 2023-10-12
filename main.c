30fb1481541cf69a0b91f961e709c76ca9f2194b
mohyangba <mohyangba@users.noreply.github.com>
2023-10-12 16:58:47 +0900

    05

--- main.c
+++ main.c
@@ -1,29 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void square1(int a)
-{
-     a= a*a;
-          printf("test number: %i\n\n", a);
+
+int factorial(int n) {
+    int i;
+    int result = 1;
+    for (i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
 }
 
-int square2(int b)
+int combination(int n, int r)
 {
-    return b*b;
+    int div1, div2; // div1=분자, div2=분모 
+    
+    div1 = factorial(n);
+    div2 = factorial(n-r)*factorial(r);
+    
+    return (div1/div2);
 }
 
-int main(){
+int main(int argc, char *argv()){
+    int n, r;
+    int div1, div2;
+    int result;
     
-    int a=2;
-    square1(a); 
+
+    // 1. 입력값
     
-    printf("a=%i\n", a);
-                     // void 의 값은 행동이 동작한 후 날라가므로 당연히 a는 2라고 나오게 된다 
+        // n입력
+    printf("input n: \n");
+    scanf("%d", &n);
     
-    int b=5;
-    b = square2(b);
-    printf("b=%i\n", b);
+        // r입력
+    printf("input r: \n");
+    scanf("%d", &r);
     
+    // 2. 분모/분자 계산
+    result = combination(n,r);
+       // n! n-r! r! 계산
+       // 분모 / 분자 계산
+     
+    // 3. 최종 값 출력
+    printf("the combination of %d and %d will be : %.21f", n, r, result);
+     // 분모/분자 나눔
+       // 결과 값 출력  
+     
   system("PAUSE");	
   return 0;
 } 
