88b706faa20468944fab2fc94fa88cddf13e883c
mohyangba <mohyangba@users.noreply.github.com>
2023-10-12 16:07:43 +0900

    03

--- main.c
+++ main.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void f1(void)
+int sumTwo(int a, int b)
 {
-     int x;
-     printf("f1 x is at %p\n", &x);
-     //&x x가 어느 메모리의 주소에 있는지 알려주는 연산자. = 변수의 메모리 주소지  
+     int sum = a + b;
+     return sum;
+     //return(a+b)로 해도 가능. 
 }
 
-int main(void){
+int square(int n)
+{
+    int squared = n*n;
+    return squared;
+    // return n*n 가능. 
+}
+
+int get_max(int x, int y)
+{
+    int max;
     
-    int x;
-    printf("main x is at %p\n", &x);
-    f1();
+    if(x<y)
+           max = y;
+    if(x>y)
+           max = x;
+    else
+        max = x = y;
+        
+    return max;
+}
+    
+int main(int argc, char *argv()){
+    
+    int a, b;
+    a=3;
+    b=9;
+    sumTwo(3, 9);
+    printf("sunTwo result will be %i\n", sumTwo(3,9));
+    
+    square(8);
+    printf("square result will be %i\n", square(8));
+    
+    int x, y; 
+    x=11;
+    y=11;
+    
+    get_max(11, 11);
+    printf("two integers will be %d and %d, so the max value will be %d.\n", x, y, get_max(11,11));
 
   system("PAUSE");	
   return 0;
-}
+} 
